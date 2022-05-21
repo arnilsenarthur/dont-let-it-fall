8e0a62ac3e4a117ac45c923d532d57504b772dc8
Árnilsen Arthur <arnilsenarthur@gmail.com>
2022-05-21 15:54:43 -0300

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Entity/TimedEntity.cs
+++ Assets/Scripts/Entity/TimedEntity.cs
@@ -14,6 +14,8 @@ namespace DontLetItFall.Entity
         public string triggerToHolder = "";
         public bool isInTrigger = false;
 
+        public GameObject expiredPlaceholder;
+
         private void OnEnable()
         {
             currentTime.value = 0;
@@ -22,7 +24,18 @@ namespace DontLetItFall.Entity
         private void Update()
         {
             if (!isInTrigger)
+            {
                 currentTime.value += Time.deltaTime;
+
+                if(currentTime.value > maxTime.value)
+                {
+                    if(expiredPlaceholder == null)
+                        return;
+
+                    GameObject spawned = Instantiate(expiredPlaceholder, this.transform.position, this.transform.rotation);
+                    Destroy(this.gameObject);
+                }
+            }
 
             float mx = maxTime.value;
             lifeBar.value = (mx - currentTime.value) / mx;
