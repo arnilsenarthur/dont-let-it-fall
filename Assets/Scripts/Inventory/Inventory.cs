6cf94e6eca3ce7c2ac147341c8cd67d0c7acda24
Árnilsen Arthur <arnilsenarthur@gmail.com>
2022-05-23 15:25:40 -0300

    Inventary

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using DontLetItFall.Variables;
+using UnityEngine;
+
+namespace DontLetItFall.Inventory
+{
+    public class Inventory : MonoBehaviour
+    {
+        public Value<int> itemsCount;
+    }
+}
